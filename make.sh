2a3df9c6bb2215679659e926aae011ed3340f9d3
werf-raspberry <sietsevdwerf@gmail.com>
2015-12-12 13:04:08 +0100

    implemented startup script

--- make.sh
+++ make.sh
@@ -1,6 +1,8 @@
 # Gebruik dit BASH script om het programma te builden/runnen na een git pull.
-STATUS = empty
+STATUS="empty"
 
 # do stuff.
+touch log.txt
+printf date > log.txt
 
-ehco "make.sh beeindigd met status: \"$STATUS\""
+echo "make.sh beeindigd met status: \"$STATUS\""
