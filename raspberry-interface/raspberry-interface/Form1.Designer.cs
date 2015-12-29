4358d8bec142573959c40165d92a45c26ab264cc
Sietsewer <sietsewer@gmail.com>
2015-12-29 03:05:47 +0100

    added clock

--- raspberry-interface/raspberry-interface/Form1.Designer.cs
+++ raspberry-interface/raspberry-interface/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.times = new System.Windows.Forms.TabPage();
+            this.weather = new System.Windows.Forms.TabPage();
+            this.temperature = new System.Windows.Forms.TabPage();
+            this.clock = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.times);
+            this.tabControl1.Controls.Add(this.weather);
+            this.tabControl1.Controls.Add(this.temperature);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(940, 487);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // times
+            // 
+            this.times.Location = new System.Drawing.Point(4, 22);
+            this.times.Name = "times";
+            this.times.Padding = new System.Windows.Forms.Padding(3);
+            this.times.Size = new System.Drawing.Size(932, 461);
+            this.times.TabIndex = 0;
+            this.times.Text = "Tijden";
+            this.times.UseVisualStyleBackColor = true;
+            // 
+            // weather
+            // 
+            this.weather.Location = new System.Drawing.Point(4, 22);
+            this.weather.Name = "weather";
+            this.weather.Padding = new System.Windows.Forms.Padding(3);
+            this.weather.Size = new System.Drawing.Size(932, 461);
+            this.weather.TabIndex = 1;
+            this.weather.Text = "Weer";
+            this.weather.UseVisualStyleBackColor = true;
+            // 
+            // temperature
+            // 
+            this.temperature.Location = new System.Drawing.Point(4, 22);
+            this.temperature.Name = "temperature";
+            this.temperature.Size = new System.Drawing.Size(932, 461);
+            this.temperature.TabIndex = 2;
+            this.temperature.Text = "Temperatuur";
+            this.temperature.UseVisualStyleBackColor = true;
+            // 
+            // clock
+            // 
+            this.clock.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.clock.AutoSize = true;
+            this.clock.Location = new System.Drawing.Point(903, 10);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(49, 13);
+            this.clock.TabIndex = 1;
+            this.clock.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 511);
+            this.Controls.Add(this.clock);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage times;
+        private System.Windows.Forms.TabPage weather;
+        private System.Windows.Forms.TabPage temperature;
+        private System.Windows.Forms.Label clock;
     }
 }
 
--- raspberry-interface/raspberry-interface/Form1.cs
+++ raspberry-interface/raspberry-interface/Form1.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace raspberry_interface
@@ -14,6 +15,34 @@ namespace raspberry_interface
         public Form1()
         {
             InitializeComponent();
+        }
+        Thread updateTimeThread;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            updateTimeThread = new Thread(updateTime);
+            updateTimeThread.Start();
+        }
+
+        private void setTime(string s)
+        {
+            clock.Text = s;
+        }
+        delegate void SetTimeCallback(string time);
+        
+        private void updateTime()
+        {
+            SetTimeCallback c = new SetTimeCallback(setTime);
+            while (true)
+            {
+                this.Invoke(c, new object[] { DateTime.Now.ToString("H:mm:ss") });
+                clock.Text = DateTime.Now.ToString("H:mm:ss");
+                Thread.Sleep(100);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            updateTimeThread.Abort();
         }
     }
 }
